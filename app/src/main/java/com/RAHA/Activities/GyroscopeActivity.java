1062daf1f483c5d9060c98cccfdca084c34d1f80
hoenza <h.entezari78@ut.ac.ir>
2021-03-26 15:30:42 +0430

    init

---
+++ app/src/main/java/com/RAHA/Activities/GyroscopeActivity.java
@@ -0,0 +1,16 @@
+package com.RAHA.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.RAHA.R;
+
+public class GyroscopeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gyroscope);
+    }
+}
---
+++ app/src/main/java/com/RAHA/Activities/MainActivity.java
@@ -0,0 +1,41 @@
+package com.RAHA.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.RAHA.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        Button btnGyroscope = (Button) findViewById(R.id.button_gyroscope);
+        Button btnGravity = (Button) findViewById(R.id.button_gravity);
+
+        View.OnClickListener handler = new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if (v == btnGyroscope) {
+                    Intent intentGyroscope = new Intent(MainActivity.this,
+                            GyroscopeActivity.class);
+                    startActivity(intentGyroscope);
+                }
+                if (v == btnGravity) {
+                    Intent intentGravity = new Intent(MainActivity.this,
+                            GravityActivity.class);
+                    startActivity(intentGravity);
+                }
+            }
+        };
+
+        btnGyroscope.setOnClickListener(handler);
+        btnGravity.setOnClickListener(handler);
+    }
+}
